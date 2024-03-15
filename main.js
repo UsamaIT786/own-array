acb252558bd48971aa9f40c5f8358e4f5e09f79a
Usama Muzammil <160235187+UsamaIT786@users.noreply.github.com>
2024-03-15 18:17:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+//Assignment !3
+//Task 13
+var tranportation = ["Honda", "Civic", "Toyota", "Caltex"];
+tranportation.map(function (itemes) { return console.log("i would like a own ".concat(itemes)); });
---
+++ main.ts
@@ -0,0 +1,4 @@
+//Assignment !3
+//Task 13
+let tranportation : String[] = ["Honda","Civic","Toyota","Caltex"];
+tranportation.map((itemes) => console.log(`i would like a own ${itemes}`) );
